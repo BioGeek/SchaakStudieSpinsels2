98daf5ab54caab7f4ecf97c563dda3b98518e695
Jeroen Van Goey <j.vangoey@instadeep.com>
2022-09-13 21:03:39 +0200

    Update SITEURL

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://schaakstudiespinsels2.be'
+SITEURL = 'https://schaakstudiespinsels2.be'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
